6c5d4224ed59312a1a392a19e652b494e48d5e6e
plusmate <psejin1478@gmail.com>
2025-01-15 13:53:20 +0900

    Lv3 구현) Kiosk클래스 구현

---
+++ src/main/java/com/example/lv3/MenuItem.java
@@ -0,0 +1,28 @@
+package com.example.lv3;
+
+public class MenuItem {
+    private String name;
+    private double price;
+    private String description;
+
+    public MenuItem() {
+    }
+
+    public MenuItem(String name, double price, String description) {
+        this.description = description;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
